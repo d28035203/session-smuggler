57711e8254a1aa4f674aee17c4c3e75a4e6926e3
d28035203 <289484834+d28035203@users.noreply.github.com>
2025-12-20 17:22:19 +0530

    refactor: add CORS, recovery, request logging, and env validation

--- app/setup.go
+++ app/setup.go
@@ -7,16 +7,43 @@ import (
 	"github.com/d28035203/legendary-succotash/database"
 	"github.com/d28035203/legendary-succotash/router"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/joho/godotenv"
 )
 
+// SetupAndRunApp loads config, connects to the database, and starts Fiber.
 func SetupAndRunApp() error {
 	_ = godotenv.Load()
+
+	required := []string{
+		"APP_HOST", "APP_PORT", "TOKEN_SECRET",
+		"POSTGRES_HOST", "POSTGRES_PORT", "POSTGRES_USER",
+		"POSTGRES_PASSWORD", "POSTGRES_DBNAME",
+	}
+	for _, key := range required {
+		if os.Getenv(key) == "" {
+			return fmt.Errorf("environment variable %s is required", key)
+		}
+	}
+
 	db, err := database.Connect()
 	if err != nil {
 		return err
 	}
-	app := fiber.New()
+
+	app := fiber.New(fiber.Config{
+		AppName: "legendary-succotash",
+	})
+	app.Use(cors.New())
+	app.Use(recover.New())
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path} ${latency}\n",
+	}))
+
 	router.SetupRoutes(app, db)
-	return app.Listen(fmt.Sprintf("%s:%s", os.Getenv("APP_HOST"), os.Getenv("APP_PORT")))
+
+	addr := fmt.Sprintf("%s:%s", os.Getenv("APP_HOST"), os.Getenv("APP_PORT"))
+	return app.Listen(addr)
 }
